35db26c8b9fe33cda22b29d6361902d4a7acd20b
WinHtunOo29 <winhtunoo12345@gmail.com>
2022-10-30 20:10:10 +0630

    CRUD commit

--- routes/web.php
+++ routes/web.php
@@ -8,14 +8,36 @@ Route::get('/', [ArticleController::class, 'index']);
 
 Route::get('/articles', [ArticleController::class, 'index']);
 
-Route::get('/articles/detail', function() {
-    return "Article Detail";
-})->name('article.detail');
-
-Route::get('/articles/more', function() {
-    return redirect()->route('article.detail');
-});
-
-Route::get('/articles/detail/{id}', [ArticleController::class, 'detail']);
+Route::get('/articles/details/{id}', [ArticleController::class, 'details']);
 
 Route::get('/products', [ProductController::class, 'index']);
+
+Route::get('/articles/add', [ArticleController::class, 'add']);
+
+Route::post('/articles/add', [ArticleController::class, 'create']);
+
+Route::get('/articles/delete/{id}', [ArticleController::class, 'delete']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
